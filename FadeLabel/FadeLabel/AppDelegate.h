308806718c2995f75d03ee23dfbdef8b4e564e3a
向云飞 <978771447@qq.com>
2015-11-11 15:19:00 +0800

    创建项目

---
+++ FadeLabel/FadeLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FadeLabel
+//
+//  Created by MyMac on 15/11/11.
+//  Copyright © 2015年 MyMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FadeLabel/FadeLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FadeLabel
+//
+//  Created by MyMac on 15/11/11.
+//  Copyright © 2015年 MyMac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
